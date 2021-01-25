050bd90a6f4c396c3b060791176f09b13c803615
DESKTOP-DQG055U\micheljacksonabilash <yupabilash@gmail.com>
2021-01-25 20:23:32 +0530

    views page created

--- gym_management/managementapp/admin.py
+++ gym_management/managementapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import customer_details, renewal, trainer_details, branch, subscription
 
 # Register your models here.
+admin.site.register(customer_details)
+admin.site.register(renewal)
+admin.site.register(trainer_details)
+admin.site.register(branch)
+admin.site.register(subscription)
--- gym_management/managementapp/views.py
+++ gym_management/managementapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import customer_details, renewal, trainer_details, branch, subscription
+from. import  views
 # Create your views here.
+def index(request):
+    num_customer =customer_details.objects.all().count()
+    num_renewal =renewal.objects.all().count()
+    num_trainer_details = trainer_details.objects().count()
+    context = {
+        'num_customer': num_customer,
+        'num_renewal' : num_renewal,
+        'num_trainer' : num_trainer_details,
+    }
+    return render(request, 'index.html', context=context)
